a531e70468b3dbbf4582835376f00e91878d60ad
Yoojung Kim <apex.yj@gmail.com>
2021-02-08 00:54:43 +0900

    #3.2. user admin + custom admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "id_card",
+                    "bio",
+                    "login_method",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "is_staff",
+        "email_verified",
+        "email_secret",
+        "login_method",
+    )
