d2cda4cdf754d8ff823675e9d1e2f12a5189e7c1
Yoojung Kim <apex.yj@gmail.com>
2021-02-12 00:25:22 +0900

    #4.9 Vet Model and Admin

--- vets/admin.py
+++ vets/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Vet)
+class VetAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "visit_date",
+        "vet_title",
+        "cat",
+        "diagnosis",
+    )
+
+    raw_id_fields = ("cat",)
+
+    ordering = (
+        "visit_date",
+        "cat",
+        "vet_title",
+    )
+
+    search_fields = ("=cat", "^vet_title")
+
+
+@admin.register(models.Vaccination)
+class VaccinationAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "visit_date",
+        "vet_title",
+        "cat",
+        "vaccination",
+    )
+
+    raw_id_fields = ("cat",)
+
+    ordering = (
+        "visit_date",
+        "cat",
+        "vet_title",
+    )
+
+    search_fields = ("=cat", "^vet_title")
--- vets/models.py
+++ vets/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Vet(core_models.TimeStampedModel):
+
+    visit_date = models.DateField()
+    vet_title = models.CharField(max_length=100)
+    cat = models.ForeignKey("cats.Cat", related_name="vets", on_delete=models.CASCADE)
+    symptom = models.CharField(max_length=200, default="")
+    diagnosis = models.CharField(max_length=200, default="")
+    expense = models.IntegerField(blank=True, null=True)
+    receipt = models.FileField(blank=True, null=True)
+
+
+class Vaccination(core_models.TimeStampedModel):
+    visit_date = models.DateField()
+    vet_title = models.CharField(max_length=100)
+    cat = models.ForeignKey("cats.Cat", related_name="vets", on_delete=models.CASCADE)
+    vaccination = models.CharField(max_length=100, blank=True, null=True)
+    expense = models.IntegerField(blank=True, null=True)
+    receipt = models.FileField(blank=True, null=True)
