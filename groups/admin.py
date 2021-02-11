319250c2ef66d46598381beab3a65adf76189cbd
Yoojung Kim <apex.yj@gmail.com>
2021-02-12 00:37:55 +0900

    #4.10 Group Model and Admin

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Group)
+class CustomGroupAdmin(GroupAdmin):
+
+    list_display = ("name", "city", "group_avatar", "count_members")
--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Group(AbstractUser):
+
+    bio = models.TextField()
+    members = models.ManyToManyField("users.User", blank=True, related_name="group")
+    city = models.CharField(max_length=80, blank=True, null=True)
+    group_avatar = models.ImageField(upload_to="group_avatars", null=True, blank=True)
+
+    def count_members(self):
+        return self.members.count()
