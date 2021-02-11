114eece8097aba74956477719c07a979b3122cb1
Yoojung Kim <apex.yj@gmail.com>
2021-02-11 23:54:24 +0900

    #4.8 List Model and Admin

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.List)
+class ListAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "user",
+        "count_cats",
+    )
+
+    search_fields = ("title",)
+
+    filter_horizontal = ("cats",)
--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class List(core_models.TimeStampedModel):
+
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        "users.User", related_name="lists", on_delete=models.CASCADE
+    )
+    cats = models.ManyToManyField("cats.Cat", related_name="lists")
+
+    def __str__(self):
+        return self.title
+
+    def count_cats(self):
+        return self.cats.count()
+
+    count_cats.short_description = "Number of Cats"
